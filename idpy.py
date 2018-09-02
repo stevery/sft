0709ee85691206e2407537a598fe372ea8ffdeab
stevery.ibm <stevery.ibm@coupang.com>
2018-09-02 14:36:51 +0900

    sft-001: idpy

--- idpy.py
+++ idpy.py
@@ -1,6 +1,7 @@
 import sys
 import optparse
-from pathlib import Path, PurePath, PurePosixPath
+import platform
+from pathlib import Path, PurePath, PurePosixPath, PureWindowsPath, WindowsPath
 import magic
 import hashlib
 import pandas as pd
@@ -9,15 +10,32 @@ from pandas import DataFrame
 
 class Idpy:
     def __init__(self, in_path):
+        self.os = ""
+        self.check_os()
         self.in_path = self.build_ab_path(in_path)
         self.files = []
 
 
-    def build_ab_path(self, path):
-        if PurePosixPath(path).is_absolute():
-            return Path(path)
+    def check_os(self):
+        ostype = platform.platform()
+        print(ostype)
+        if 'Windows' in ostype:
+            self.os = 'windows'
         else:
-            return PurePath(Path.cwd(),path)
+            self.os = 'linux'
+
+
+    def build_ab_path(self, path):
+        if self.os == 'windows':
+            if PureWindowsPath(path).is_absolute():
+                return Path(path)
+            else:
+                return PurePath(Path.cwd(),path)
+        else:
+            if PurePosixPath(path).is_absolute():
+                return Path(path)
+            else:
+                return PurePath(Path.cwd(),path)
 
 
     def map_all_files(self, path=None):
@@ -42,18 +60,20 @@ def main():
         sys.exit(0)
     else:
         ip = Idpy(path)
+        print(path)
         ip.map_all_files()
         result = {}
         for file in ip.files:
+            print(file)
             tmp = {file:{}}
             tmp[file].update({'filename':PurePosixPath(file).stem+''.join(PurePosixPath(file).suffixes)})
             tmp[file].update({'size':Path(file).stat().st_size})
             tmp[file].update({'extension':''.join(PurePosixPath(file).suffixes)})
             tmp[file].update({'st_date':Path(file).stat().st_mtime})
             tmp[file].update({'type':magic.from_file(str(file))})
-            tmp[file].update({'md5':hashlib.md5(open(file, 'rb').read()).hexdigest()})
-            tmp[file].update({'sha1':hashlib.sha1(open(file, 'rb').read()).hexdigest()})
-            tmp[file].update({'sha256':hashlib.sha256(open(file, 'rb').read()).hexdigest()})
+            tmp[file].update({'md5':hashlib.md5(open(str(file), 'rb').read()).hexdigest()})
+            tmp[file].update({'sha1':hashlib.sha1(open(str(file), 'rb').read()).hexdigest()})
+            tmp[file].update({'sha256':hashlib.sha256(open(str(file), 'rb').read()).hexdigest()})
 
             result.update(tmp)
             # print(tmp)
